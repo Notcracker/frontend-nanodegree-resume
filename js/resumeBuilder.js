41a9a591c417468ee57d496dea6372624ab5d843
Miri <kuroyujo@gmail.com>
2016-01-17 13:40:25 +0300

    4 objects: bio, education, etc in resume.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,42 +1,84 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
+var work ={
+	"jobs":[
+	{
+		"employer":"mcdonalds",
+		"title":"cashier",
+		"location":"Saint Petersburg",
+		"dates":"1998-2016",
+		"description":"funny days of my life"
+		},
+		{
+			"employer":"Zoya",
+			"title":"promoter",
+			"location":"Saint Petersburg",
+			"dates":"2012-2016",
+			"description":"so lonly girl..."
+			}]
+}
 
 
-var awesomeThoughts = "I am Miri and I am awesome!" 
-console.log(awesomeThoughts);
-var funThoughts = awesomeThoughts.replace("awesome","fun");
-/*$("#main").append(funThoughts);
+var projects = {
+	"projects":[
+	{
+		"title":"autumn",
+		"dates":"1998-2016",
+		"description":"a state",
+		"images":[
+		"http://dreamatico.com/data_images/autumn/autumn-5.jpg",
+		"http://jocotours.com/wp-content/uploads/2013/06/AutumnBanner21.jpg",
+		"http://cdn.playbuzz.com/cdn/d507c548-bb76-4f2c-8333-0375e3172372/7a3cd493-c3e9-4b84-bec9-6431d9bdba20.jpg"
+		]
+		}]
+}
 
-var formattedName = HTMLheaderName.replace("%data%","Miri");
-var formattedRole = HTMLheaderRole.replace("%data%","question");
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-skills = [1,2,3];
-var bio= {
+var bio = {
 	"name":"Miri",
-	"role":"Mascot",
-	"contact": "telephone",
-	"pic":"<img src='images/fry.jpg'>" ,
-	"welcome":"hi!",
-	"skills":skills};
+	"role":"mascot",
+	"welcomeMessage":"You are welcome!",
+	"contacts":{
+		"mobile":"+7-965-763-57-34",
+		"email":"waver-li@riseup.net",
+		"github":"Notcracker"
+	},
+	"skills":[
+	"miriness",
+	"awesomeness",
+	"Nness"]
+}
 
-
-$("#main").append(bio.pic);
-$("#main").append(bio.skills);
-$("#main").append(bio.contact, bio.welcome, bio.role);
-
-bio.work = {"position":"lowwy", "city":"St. Petersburg"}
-bio["education"] = {"name":"iog", "year":"1986"}
-
-$("#main").append(bio.work["position"],bio.education.name)
-*/
-
-var education ={
+var education = {
 	"schools":[
-	{"art":"lycium",
-	"city":"st"},
-	{"math":"lycium",
-	"city":"st"}],
-
-};
+	{
+		"name":"96",
+		"location":"Saint Petersburg",
+		"degree":"schooller",
+		"majors":[],
+		"dates":"2005-2010",
+		"url":"http://school96.spb.ru/"
+		},
+		{
+			"name":"художественный лицей имени иогансона",
+			"location":"Saint Petersburg",
+			"degree":"graduate",
+			"majors":[
+			"art",
+			"pictorial art"
+			],
+			"dates":"2010-2012",
+			"url":"https://ru.wikipedia.org/wiki/%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B3%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%B0%D1%8F_%D1%81%D1%80%D0%B5%D0%B4%D0%BD%D1%8F%D1%8F_%D1%85%D1%83%D0%B4%D0%BE%D0%B6%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%B0%D1%8F_%D1%88%D0%BA%D0%BE%D0%BB%D0%B0"
+			}],
+	"onlineCourses":[
+	{
+		"title":"student",
+		"school":"udacity",
+		"dates":"2015",
+		"url":"https://www.udacity.com/courses/cs101"
+		},
+		{
+			"title":"student",
+			"school":"udacity",
+			"dates":"2016",
+			"url":"https://www.udacity.com/courses/ud804"
+			}]
+}
